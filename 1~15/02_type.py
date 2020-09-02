26a84dd3a1b4501dfd7bdda352a01ab25301dc28
zth <test@gmail.com>
2020-09-02 08:58:28 +0800

    add

---
+++ 1~15/02_type.py
@@ -0,0 +1,22 @@
+"""
+将华氏温度转换为摄氏温度
+
+Version: 0.1
+Author: 骆昊
+"""
+
+
+# 温度转换
+# f = float(input('请输入华氏温度: '))
+# c = (f - 32) / 1.8
+# print('%.1f华氏度 = %.1f摄氏度' % (f, c))
+
+
+# 输入圆的半径计算计算周长和面积。
+# radius = float(input('请输入圆的半径: '))
+# perimeter = 2 * 3.1416 * radius
+# area = 3.1416 * radius * radius
+# print('周长: %.2f' % perimeter)
+# print('面积: %.2f' % area)
+
+
