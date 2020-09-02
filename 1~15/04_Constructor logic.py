939e1be0e1d4eef7e04ea7b27d9a2291c5b67917
zth <test@gmail.com>
2020-09-02 21:35:04 +0800

    add

---
+++ 1~15/04_Constructor logic.py
@@ -0,0 +1,36 @@
+'''
+Date: 2020-09-02
+Author: Wumo
+'''
+
+
+# for num in range(100, 1000):
+#     low = num % 10
+#     mid = num // 10 % 10
+#     high = num // 100
+#     if num == low ** 3 + mid ** 3 + high ** 3:
+#         print(num)
+
+
+
+"""
+正整数的反转
+"""
+
+# num = int(input('num = '))
+# reversed_num = 0
+# while num > 0:
+#     reversed_num = reversed_num * 10 + num % 10
+#     num //= 10
+# print(reversed_num)
+
+
+"""
+《百钱百鸡》问题
+"""
+
+for x in range(0, 20):
+    for y in range(0, 33):
+        z = 100 - x - y
+        if 5 * x + 3 * y + z / 3 == 100:
+            print('公鸡: %d只, 母鸡: %d只, 小鸡: %d只' % (x, y, z))
