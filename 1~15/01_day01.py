7b113a1ab3d8e6d09db0e5f14a9881970a52fc57
zth <test@gmail.com>
2020-09-02 08:56:40 +0800

    first commit

---
+++ 1~15/01_day01.py
@@ -0,0 +1,21 @@
+import sys
+
+# print(sys.version_info)
+# print(sys.version)
+
+# print('hello, world!')
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('black')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
