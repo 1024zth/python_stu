9d1f917bb1d4a2c93267327f810a5d660e0f8d2a
zth <test@gmail.com>
2020-09-22 10:09:46 +0800

    add

--- 1~15/08.py
+++ 1~15/08.py
@@ -3,6 +3,9 @@ import random
 
 class Card(object):
     """一张牌"""
+    '''
+    属性：花色，值
+    '''
 
     def __init__(self, suite, face):
         self._suite = suite
@@ -64,7 +67,7 @@ class Poker(object):
 
     @property
     def has_next(self):
-        """还有没有牌"""
+        '''判断是否有剩余牌'''
         return self._current < len(self._cards)
 
 
@@ -87,28 +90,32 @@ class Player(object):
         """摸牌"""
         self._cards_on_hand.append(card)
 
-    def arrange(self, card_key):
-        """玩家整理手上的牌"""
+    def arrange(self,card_key):
+        '''整理牌'''
         self._cards_on_hand.sort(key=card_key)
 
 
-# 排序规则-先根据花色再根据点数排序
-def get_key(card):
-    return (card.suite, card.face)
+# # 排序规则-先根据花色再根据点数排序
+# def get_key(card):
+#     return (card.suite, card.face)
 
 
 def main():
     p = Poker()
     p.shuffle()
     players = [Player('东邪'), Player('西毒'), Player('南帝'), Player('北丐')]
-    for _ in range(13):
-        for player in players:
-            player.get(p.next)
+    while p.has_next:
+        for i in range(13):
+            for player in players:
+                player.get(p.next)
     for player in players:
         print(player.name + ':', end=' ')
-        player.arrange(get_key)
+        player.arrange(lambda card: (card.suite,card.face))
         print(player.cards_on_hand)
-
-
+        # for _ in range(len(player.cards_on_hand)):
+        #     current = 0
+        #     print(player.cards_on_hand.pop(current), end=' ')
+        #     current += 1
+        # print('\n')
 if __name__ == '__main__':
     main()
